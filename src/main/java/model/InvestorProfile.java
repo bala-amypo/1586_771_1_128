1afde528381db350fe38ee38955380158066f420
bala-amypo <bala@amypo.in>
2025-12-16 08:41:34 +0000

    User commit

--- src/main/java/model/InvestorProfile.java
+++ src/main/java/model/InvestorProfile.java
@@ -1,3 +1,10 @@
 package com.example.demo.model;
 
-class
+public class InvestorProfile{
+    private long id;
+    private String investorld;
+    private String fullname;
+    private String email;
+    private Boolean active;
+    private LocalDateTime createdAt;
+}
