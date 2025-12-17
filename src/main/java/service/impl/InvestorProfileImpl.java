e847ca03c3588db6568c17f1d210f9c78609f3d7
bala-amypo <bala@amypo.in>
2025-12-17 10:11:21 +0000

    User commit

---
+++ src/main/java/service/impl/InvestorProfileImpl.java
@@ -0,0 +1,45 @@
+package com.demo.service.impl;
+
+import com.demo.model.InvestorProfile;
+import com.demo.repository.InvestorProfileRepository;
+import com.demo.service.InvestorProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class InvestorProfileServiceImpl implements InvestorProfileService {
+
+    @Autowired
+    private InvestorProfileRepository investorRepo;
+
+    @Override
+    public InvestorProfile createInvestor(InvestorProfile investor) {
+        return investorRepo.save(investor);
+    }
+
+    @Override
+    public InvestorProfile getInvestorById(Long id) {
+        return investorRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Investor not found"));
+    }
+
+    @Override
+    public InvestorProfile findByInvestorId(String investorId) {
+        return investorRepo.findByInvestorId(investorId);
+    }
+
+    @Override
+    public List<InvestorProfile> getAllInvestors() {
+        return investorRepo.findAll();
+    }
+
+    @Override
+    public void updateInvestorStatus(Long id, boolean active) {
+        InvestorProfile investor = getInvestorById(id);
+        investor.setActive(active);
+        investorRepo.save(investor);
+    }
+}
