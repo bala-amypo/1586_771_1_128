9d3d85abfe072aee7d212f8a31d94e813d04a2eb
bala-amypo <bala@amypo.in>
2025-12-18 08:36:21 +0000

    User commit

--- src/main/java/repository/AllocationSnapshotRecordRepository.java
+++
@@ -1,9 +0,0 @@
-// package com.example.project.repository;
-
-// import com.example.project.model.AllocationSnapshotRecord;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-
-// @Repository
-// public interface AllocationSnapshotRecordRepository extends JpaRepository<AllocationSnapshotRecord, Long> {
-// }
--- src/main/java/repository/AssetClassAllocationRuleRepository.java
+++
@@ -1,9 +0,0 @@
-// package com.example.project.repository;
-
-// import com.example.project.model.AssetClassAllocationRule;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-
-// @Repository
-// public interface AssetClassAllocationRuleRepository extends JpaRepository<AssetClassAllocationRule, Long> {
-// }
