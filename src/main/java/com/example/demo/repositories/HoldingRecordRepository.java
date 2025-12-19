eef1178562a82e4a19d83a968770f86fa40ed7ab
bala-amypo <bala@amypo.in>
2025-12-19 04:51:48 +0000

    User commit

--- src/main/java/com/example/demo/repositories/HoldingRecordRepository.java
+++ src/main/java/com/example/demo/repositories/HoldingRecordRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface HoldingRecordRepository extends JpaRepository<HoldingRecord, Long> {
     List<HoldingRecord> findByInvestorId(Long investorId);
     List<HoldingRecord> findByValueGreaterThan(Double value);
-    List<HoldingRecord> findByInvestorAndAssetClass(Long investorId, AssetClassType assetClass);
+    List<HoldingRecord> findByInvestorIdAndAssetClass(Long investorId, AssetClassType assetClass);
 }
