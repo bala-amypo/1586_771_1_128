022f1ff5b5ce3ebcf571e061813d9057f1d5bff6
bala-amypo <bala@amypo.in>
2025-12-29 09:10:14 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,10 +1,32 @@
 package com.example.demo.security;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
+
     @Override
-    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String s) {
-        return null;
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        // TEMPORARY STATIC USER (for testing)
+        // email = jesil@gmail.com
+        // password = jesil
+
+        if (!email.equals("jesil@gmail.com")) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new User(
+                "jesil@gmail.com",
+                "{noop}jesil",   // {noop} = no password encoding
+                Collections.emptyList()
+        );
     }
 }
